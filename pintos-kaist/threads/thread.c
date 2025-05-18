dec4e3a1899a0fdbe94b8e51b4bde9c1c6adddd0
SinMoonGh <tity0009@naver.com>
2025-05-18 15:06:23 +0900

    test_max_priority 수정 .vscode 디버깅용 launch.json 수정

--- pintos-kaist/threads/thread.c
+++ pintos-kaist/threads/thread.c
@@ -670,8 +670,13 @@ test_max_priority (void)
 
     struct thread *th = list_entry(list_front(&ready_list), struct thread, elem);
 
-    if (thread_get_priority() < th->priority)
-        thread_yield();
+    if (thread_current()->priority < th->priority) {
+        /** Project 2: Panic 방지 */
+        if (intr_context())
+            intr_yield_on_return();
+        else
+            thread_yield();
+    }
 }
 void up_recent(){
 	struct thread *t = thread_current();
